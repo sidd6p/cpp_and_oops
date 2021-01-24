0b4ad73d56c5bdd4d8e9a080885a706a8b0bfd4e
siddhartha <siddpurwar@gmail.com>
2021-01-24 22:26:04 +0530

    done with constructor and destructors

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,15 +1,25 @@
- #include<iostream>
+#include<iostream>
 #include"Object_class.h"
 using namespace std;
+
 int main(){
+
   cout<<"Started"<<endl;
+
   customers c1;
   customers *c2 = new customers();
-
+  customers();//When the constructor is called explicitly the compiler creates a nameless temporary object and it is immediately destroyed.
+  customers c5("sidd", 10000);
   c1.set_customer_name("siddhartha");
   c1.set_customer_amount(100);
   c2->set_customer_name("sarthak");
   c2->set_customer_amount(1000);
+  //a→b is essentially a shorthand notation for (*a).b,
+  //if a is a pointer to an object, then a->b is accessing the property b of the object that a points to.
+
+  customers c3 = c1;//copy constructor
+  customers c4;
+  c4 = (*c2);//assignment operator
 
   cout<<c1.get_customer_name()<<"\t";
   cout<<c1.get_customer_amount();
@@ -17,10 +27,19 @@ int main(){
   cout<<c2->get_customer_name()<<"\t";
   cout<<c2->get_customer_amount();
   cout<<"\n";
-  cout<<"Total number of customers = "<<customers::total_customer();
+  cout<<c3.get_customer_name()<<"\t";
+  cout<<c3.get_customer_amount();
+  cout<<"\n";
+  cout<<c4.get_customer_name()<<"\t";
+  cout<<c4.get_customer_amount();
+  cout<<"\n";
+  cout<<c5.get_customer_name()<<"\t";
+  cout<<c5.get_customer_amount();
+  cout<<"\n";
 
-  c1.~customers();
-  c2->~customers();
-  delete c2;
+  cout<<"Total number of customers = "<<customers::total_customer()<<endl;
+
+  c1.~customers();//because of the explicit call to destructor, the destructor for c1 will be called twice.
+  delete c2;//this will call destructor automatically, destructor will be called only once for object c2, since it is explicitly allocated in the memory.
   return 0;
 }
--- cpp/object_class.h
+++ cpp/object_class.h
@@ -2,16 +2,24 @@ using namespace std;
 class customers;
 
 class customers{
+
   string name;
   int amount;
   static int customers_number;
+
   public:
+
   customers();
+  customers(string , int);//constructor overloading
   void set_customer_name(string name);
   void set_customer_amount(int amount);
   string get_customer_name();
   int get_customer_amount();
   static int total_customer();
+  //A static member function differs from a regular member function in that it can be called without an instance of a class,
+  //and since it has no instance, it cannot access non-static members of the class.
+  customers(const customers &old_obj);
+  //there is an implicit copy constructor for every class.
   ~customers();
 };
 
@@ -19,24 +27,45 @@ int customers :: customers_number = 0;
 
 customers :: customers(){
   name =  "your_name";
+  amount = 0;
   customers_number = customers_number + 1;
+  cout<<"constructor is called\n";
 }
+
+customers :: customers(string name, int amount){
+    this->name = name;
+    this->amount = amount;
+    customers_number = customers_number + 1;
+}
+
 void customers :: set_customer_name(string name){
   this->name = name;
 }
+
 void customers :: set_customer_amount(int amount){
   this->amount = amount;
 }
+
 string customers :: get_customer_name(){
     return (this->name);
 }
+
 int customers :: get_customer_amount(){
     return(this->amount);
 }
+
+customers :: customers(const customers &old_obj){
+    this->name = old_obj.name;
+    this->amount = old_obj.amount;
+    customers_number = customers_number + 1;
+    cout<<"copy constructor is called\n";
+}
+
 customers :: ~customers(){
     this->name = "enter_your_name";
     this->amount = 0;
     --customers_number;
+    cout<<"destructor is called\n";
 }
 
 int customers :: total_customer(){
