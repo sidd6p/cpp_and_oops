c9be57dcbf1756a2751e67e73a107e4544384e01
siddhartha purwar <siddpurwar6@gmail.com>
2021-01-28 13:29:25 +0530

    done with diamond problem

--- cpp/main2.cpp
+++ cpp/main2.cpp
@@ -2,15 +2,19 @@
 Destructor of ‘great_p’ will also be called two times when object ‘child_1’ is destructed.
 So object ‘child_1’ has two copies of all members of ‘great_p’, this causes ambiguities.*/
 /*when we will use virtual, then default constuctor of great_p is called only.....not any paramrtric constructor*/
-
+/*In general, it is not allowed to call the grandparent’s constructor directly,
+it has to be called through parent class. It is allowed only when ‘virtual’ keyword is used.*/
 #include<iostream>
 using namespace std;
 
 class great_p{
-  int i = 10;
+  int i = 1000;
 public:
   great_p(){
     cout<<"constructor of great_p\n";
+  }
+  great_p(int a){
+    cout<<"constructor(paramrtric) of great_p\n";
   }
   void show(){
     cout<<i;
@@ -19,27 +23,53 @@ public:
 
 class p1 :  virtual public great_p{
 public:
-  p1() : great_p(){
+  p1(int z) : great_p(z){
     cout<<"constructor of p1 is called\n";
   }
 };
 
 class p2 : virtual public great_p{
 public:
-  p2() : great_p(){
+  p2(int y) : great_p(y){
     cout<<"constructor of p2 is called\n";
   }
 };
 
 class c1 : public p1, public p2{
 public:
-  c1() : p1(), p2(){
+  c1(int x) : p1(x), p2(x), great_p(x){//in this way we can call paramrtric constructor of parent and grand_parent
     cout<<"constructor of c1 is called\n";
   }
 };
 
+
+class A
+{
+  int x;
+public:
+  void setX(int i) {x = i;}
+  void print() { cout << x; }
+};
+
+class B: virtual public A
+{
+public:
+  B()  { setX(10); }
+};
+
+class C: virtual public A
+{
+public:
+  C()  { setX(20); }
+};
+
+class D: public C, public B {//this will set x = 10, because it will call constructor of "C" and then the constructor of "B".
+};
+
 int main(){
-  c1 child_1;
- // child_1.show();
+  c1 child_1(10);
+  child_1.show();
+  D d;
+  d.print();
   return 0;
 }
