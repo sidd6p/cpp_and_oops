8be093038f70a80114009ba0cb35a14aae0401f2
siddhartha purwar <siddpurwar6@gmail.com>
2021-01-28 23:30:30 +0530

    done with basic of virtual function.....complex concept are still going on

--- cpp/main3.cpp
+++ cpp/main3.cpp
@@ -1,25 +1,77 @@
+//https://www.geeksforgeeks.org/virtual-destructor/
+//https://www.geeksforgeeks.org/virtual-functions-and-runtime-polymorphism-in-c-set-1-introduction/
 #include <iostream>
 using namespace std;
-class A
-{
- public:
- virtual void display()
- {
-  cout << "Base class is invoked"<<endl;
- }
+
+class Base {
+   public:
+    virtual void print() {
+        cout << "Base Function" << endl;
+    }
 };
-class B:public A
-{
- public:
- void display()
- {
-  cout << "Derived Class is invoked"<<endl;
- }
+
+class Derived : public Base {
+   public:
+    void print() {
+        cout << "Derived Function" << endl;
+    }
 };
-int main()
-{
- A* a;    //pointer of base class
- B b;     //object of derived class
- a = &b;
- a->display();   //Late Binding occurs
+
+// CPP program to illustrate
+// working of Virtual Functions
+#include <iostream>
+using namespace std;
+
+class base {
+public:
+    void fun_1() { cout << "base-1\n"; }
+    virtual void fun_2() { cout << "base-2\n"; }
+    virtual void fun_3() { cout << "base-3\n"; }
+    virtual void fun_4() { cout << "base-4\n"; }
+};
+
+class derived : public base {
+public:
+    void fun_1() { cout << "derived-1\n"; }
+    void fun_2() { cout << "derived-2\n"; }
+    void fun_4(int x) { cout << "derived-4\n"; }
+};
+
+
+int main() {
+    Derived derived1;
+    // pointer of Base type that points to derived1
+    Base* base1 = &derived1;
+    // calls member function of Derived class
+    base1->print();
+
+    /*Base base2;
+    Derived *derived2 = base2;
+    derived2 ->print();*/
+
+
+    base* p;
+    derived obj1;
+    p = &obj1;
+
+    // Early binding because fun1() is non-virtual
+    // in base
+    p->fun_1();
+
+    // Late binding (RTP)
+    p->fun_2();
+
+    // Late binding (RTP)
+    p->fun_3();
+
+    // Late binding (RTP)
+    p->fun_4();
+
+    // Early binding but this function call is
+    // illegal(produces error) becasue pointer
+    // is of base type and function is of
+    // derived class
+    // p->fun_4(5);
+
+    return 0;
 }
